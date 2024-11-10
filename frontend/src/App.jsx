8ce369fdffac3fb9b760eea79b11b1859ee8bc52
hakimsmox <abdelhakimsmox@gmail.com>
2024-11-10 01:36:39 +0100

    heder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
-import ButtonGraidient from './assets/svg/ButtonGradient'
-import Header from './components/Header';
+import ButtonGraidient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
+        <Hero />
       </div>
       <ButtonGraidient />
     </>
